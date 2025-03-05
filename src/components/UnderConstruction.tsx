3da1ad4c1fa75f1eb9e1c289eb7a5d9245c45485
Jhon Barry Gutang <jbgutang771@gmail.com>
2025-03-05 21:46:56 +0800

    resolving image not rendered in vercel

--- src/components/UnderConstruction.tsx
+++ src/components/UnderConstruction.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const UnderConstruction = () => {
     return ( <div>
         <div className="flex justify-center">
-        <Image src='/under_construction.png' alt="" width={500} height={500}></Image>
+        <Image src='/thumbnail/under_construction.png' alt="" width={500} height={500}></Image>
         </div>
         <div className="text-3xl">
             Sorry, Website still under construction
