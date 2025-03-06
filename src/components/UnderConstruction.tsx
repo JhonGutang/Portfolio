d9eff215f935d38166913617c03787cb15fdb68d
Jhon Barry Gutang <jbgutang771@gmail.com>
2025-03-06 13:09:35 +0800

    modified to ensure shadcn works

---
+++ src/components/UnderConstruction.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const UnderConstruction = () => {
+    return ( <div>
+        <div className="flex justify-center">
+        <Image src='/thumbnail/under_construction.png' alt="" width={500} height={500}></Image>
+        </div>
+        <div className="text-3xl">
+            Sorry, Website still under construction
+        </div>
+    </div> );
+}
+ 
+export default UnderConstruction;
